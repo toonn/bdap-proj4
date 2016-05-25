4711ef7acc8d7b8ffc049dee607cea4be0b8ca3f
Toon Nolten <toon.nolten@student.kuleuven.be>
2016-05-25 20:13:33 +0200

    fixed syntax errors and updated pom for hadoop-core

--- src/MapReduceTripLength.java
+++ src/MapReduceTripLength.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.Scanner;
-import java.util.Locale;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
@@ -11,7 +8,7 @@ import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
 public class MapReduceTripLength {
-  public static class Map extends MapReduceBase implements Mapper<LongWriteable, Text, Text, IntWritable> {
+  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
