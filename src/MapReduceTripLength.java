a7d81cade3e8c0707d0d7751811573e70997d2b9
Toon Nolten <toon.nolten@student.kuleuven.be>
2016-05-27 17:36:07 +0200

    figuring out how to get single file output from mapreduce; no reduce, getmerge?

--- src/MapReduceTripLength.java
+++ src/MapReduceTripLength.java
@@ -8,26 +8,37 @@ import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
 public class MapReduceTripLength {
-  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-      String line = value.toString();
-      StringTokenizer tokenizer = new StringTokenizer(line);
-      while (tokenizer.hasMoreTokens()) {
-        word.set(tokenizer.nextToken());
-        output.collect(word, one);
-      }
-    }
+
+  /**
+   * Calculates the distance in kilometers, input in radians
+   */
+  private static double sphereDistance(double lat1, double lon1, double lat2, double lon2) {
+    double R = 6371009; // kilometers
+    double dPhi = lat1 - lat2;
+    double dLambda = lon1 - lon2;
+    double phi_m = (lat1 + lat2) / 2;
+    double D = R * Math.sqrt(Math.pow(dPhi, 2) + Math.pow(Math.cos(phi_m) * dLambda, 2));
+    return D;
   }
 
-  public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-      int sum = 0;
-      while (values.hasNext()) {
-        sum += values.next().get();
+  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, String, Double> {
+    public void map(LongWritable key, Text value, OutputCollector<String, Double> output, Reporter reporter) throws IOException {
+      String line = value.toString();
+      String[] trip = line.split("\\s+");
+      if (trip.length == 7) {
+        // Only need the positions but only valid trips count
+        try {
+          double lat1 = Double.parseDouble(trip[1]);
+          double lon1 = Double.parseDouble(trip[1]);
+          double lat2 = Double.parseDouble(trip[1]);
+          double lon2 = Double.parseDouble(trip[1]);
+
+          output.collect("TheKey",sphereDistance(lat1, lon1, lat2, lon2));
+        } catch (Exception e) {
+          // Ignore exception on the assumption that the data in the file was
+          // invalid on this line (bad practice)
+        }
       }
-      output.collect(key, new IntWritable(sum));
     }
   }
 
@@ -37,8 +48,7 @@ public class MapReduceTripLength {
     conf.setOutputKeyClass(Text.class);
     conf.setOutputValueClass(IntWritable.class);
     conf.setMapperClass(Map.class);
-    conf.setCombinerClass(Reduce.class);
-    conf.setReducerClass(Reduce.class);
+    conf.setNumReduceTasks(0);
     conf.setInputFormat(TextInputFormat.class);
     conf.setOutputFormat(TextOutputFormat.class);
     FileInputFormat.setInputPaths(conf, new Path(args[0]));
