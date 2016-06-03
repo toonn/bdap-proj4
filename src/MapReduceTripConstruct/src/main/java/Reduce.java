97ed393de5985359244341c709d4278e5a05a6d2
Toon Nolten <toon.nolten@student.kuleuven.be>
2016-06-03 22:36:45 +0200

    Reduction is complicated because segments are not guaranteed to be in order

--- src/MapReduceTripConstruct/src/main/java/Reduce.java
+++ src/MapReduceTripConstruct/src/main/java/Reduce.java
@@ -9,6 +9,35 @@ import static distance.Distance.*;
 
 public class Reduce extends Reducer<LongWritable, Segment, NullWritable, Text> {
   public void Reduce(LongWritable key, Iterable<Segment> values, Context context) throws IOException {
+    List<Segment> partialTrips = new ArrayList<Segment>();
+    boolean matched;
+    for (Segment segment : values) {
+      matched = false;
+      for (Segment partialTrip : partialTrips) {
+        if (partialTrip.match(segment)) {
+          partialTrip.merge(segment);
+          matched = true;
+        }
+      }
+      if (!matched) {
+        partialTrips.add(segment);
+      }
+    }
+    // This doesn't solve the problem start middle and end are now potentially
+    // longer... :-(
+    List<Segment> trips = new ArrayList<Segment>();
+    for (Segment partialTrip : partialTrips) {
+      matched = false;
+      for (Segment trip : trips) {
+        if (trip.match(partialTrip)) {
+          trip.merge(partialTrip);
+          matched = true;
+        }
+      }
+      if (!matched) {
+        trips.add(partialTrip);
+      }
+    }
   }
 }
 
